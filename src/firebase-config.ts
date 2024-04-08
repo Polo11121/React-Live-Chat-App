4f9f4777c14fcdc67a5abc7832d312fbf43e9736
michal <michal.jasinski11121@gmail.com>
2024-04-08 23:07:01 +0200

    Use env variables

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: `${process.env.REACT_APP_API_KEY}`,
-  authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
-  projectId: "live-chat-app-6ea25",
-  storageBucket: "live-chat-app-6ea25.appspot.com",
-  messagingSenderId: "996372925383",
-  appId: "1:996372925383:web:8b135af4e34f5ec1da27e2",
-  measurementId: "G-SEYLTVJRY6",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
